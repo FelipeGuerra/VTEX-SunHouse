6667a6066dfbbf3596be814abaa6cfad4ad645fe
Felipe Martins Guerra <felipemartinsguerra@gmail.com>
2012-08-17 18:07:25 -0300

    18:10 - 17/08/2012

---
+++ WebDev/js/user.js
@@ -0,0 +1,58 @@
+﻿$(document).ready(function() {
+
+	$("body.checkout .searchWrapper:last, body.minha-conta .searchWrapper:last, body.fale-conosco .searchWrapper:last, body.sku .searchWrapper:last").addClass('bottomSearchWrapper');
+
+	if ($("body").attr("class") == "carrinho")
+	{
+		$("iframe").attr( "scrolling", "no");
+		$("iframe").attr( "style", "overflow : hidden; width : 640px; height : 400px;");
+		$("iframe").parent().attr( "style", "overflow : visible; width : 640px; height : 400px;");
+		
+		$("a.thickbox.tbstarted").click ( function ()
+		{
+			$("iframe").attr( "scrolling", "no");
+			$("iframe").attr( "style", "overflow : hidden; width : 640px; height : 400px;");
+			$("iframe").parent().attr( "style", "overflow : visible; width : 640px; height : 400px;");
+		});
+		
+		if ( $.browser.safari )
+		{
+			$("iframe").parent().css({ "width" : "600px", "height" : "400px", "overflow" : "hidden" });
+			$("iframe").css({ "width" : "600px", "height" : "450px", "overflow" : "hidden" });
+		}
+	};
+	
+	if ($("body").attr("class") == "checkout")
+	{
+		$("iframe").attr( "scrolling", "no");
+		$("iframe").attr( "style", "overflow : hidden; width : 640px; height : 400px;");
+		$("iframe").parent().attr( "style", "overflow : visible; width : 640px; height : 400px;");
+		
+		$("a.thickbox.tbstarted").click ( function ()
+		{
+			$("iframe").attr( "scrolling", "no");
+			$("iframe").attr( "style", "overflow : hidden; width : 640px; height : 400px;");
+			$("iframe").parent().attr( "style", "overflow : visible; width : 640px; height : 400px;");
+		});
+		
+		if ( $.browser.safari )
+		{
+			$("iframe").parent().css({ "width" : "600px", "height" : "400px", "overflow" : "hidden" });
+			$("iframe").css({ "width" : "600px", "height" : "450px", "overflow" : "hidden" });
+		}
+	};
+	
+	$('#wrapAppendBody').prependTo('body');
+	$('.footer').appendTo('body');
+	$('.searchWrapper:first fieldset.search').appendTo('#header #search');
+	$('.menuwrap').appendTo('#wrapHeader #menu');
+	$('.bottomSearchWrapper .searchWrapper .search').appendTo('#footerSearch');
+	$('<div id="content_shadow"></div>').insertAfter('.content');
+
+});
+
+$(document).ajaxStop(function(){
+
+	$('.welcome').prependTo('#topBar > div');
+
+});
